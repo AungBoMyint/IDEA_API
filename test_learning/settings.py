82c5f563856a3e6a6df62a9d9e042132d5eb6ef3
AungBoMyint <aungbomyint292@gmail.com>
2024-03-21 13:37:07 +0630

    add allow host digit server

--- test_learning/settings.py
+++ test_learning/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '10.0.2.2',
     'kznidea.online',
-    'dea-api-gzdcu.ondigitalocean.app',
+    'idea-api-gzdcu.ondigitalocean.app',
     #'139.59.99.129'
 ]
 
